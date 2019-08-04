2744374fe3dc1bdda966fcc754ed7815f2095eb2
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-08-04 11:15:15 -0300

    chore: create date and time selection page

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,15 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '~/services/api';
 import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
 
-// import { Container } from './styles';
+import { Container, HoursList, Hour, Title } from './styles';
 
-function SelectDateTime() {
-  return <Background />;
+function SelectDateTime({ navigation }) {
+  const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
+
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    });
+  }
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+
+        <HoursList
+          data={hours}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              enabled={item.available}
+              onPress={() => handleSelectHour(item.value)}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
-SelectDateTime.navigationOptions = {
+SelectDateTime.navigationOptions = ({ navigation }) => ({
   title: 'Selecione o horário',
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
+
+SelectDateTime.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+    navigate: PropTypes.func,
+  }).isRequired,
 };
 
 export default SelectDateTime;
